09443f485e3492ba822cbe1db031b47849f831aa
MatejSmycka <45017046+MatejSmycka@users.noreply.github.com>
2022-07-14 17:03:17 +0200

    Update docker_overleaf_entry.sh

--- docker_overleaf_entry.sh
+++ docker_overleaf_entry.sh
@@ -1,7 +1,11 @@
-bash -c "/sbin/my_init --no-kill-all-on-exit"
+bash -c "/sbin/my_init --no-kill-all-on-exit" &
 mkdir /var/texliveonfly_dir
 cd /var/texliveonfly_dir
+wait
+echo "waiting for init"
 tlmgr update --self
 tlmgr install texliveonfly
+echo "tlmgr is OK"
 wget https://raw.githubusercontent.com/MatejSmycka/Docker-overleaf/main/dependencies.tex
+echo "installing dependencies"
 texliveonfly dependencies.tex
