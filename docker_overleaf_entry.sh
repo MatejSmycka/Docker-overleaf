ad2e458e611ec5a04bf090a7484894284fd55bbe
MatejSmycka <45017046+MatejSmycka@users.noreply.github.com>
2022-07-14 11:42:24 +0200

    Update docker_overleaf_entry.sh

--- docker_overleaf_entry.sh
+++ docker_overleaf_entry.sh
@@ -1,7 +1,7 @@
 apt-get install -y wget
 mkdir /texliveonfly_dir
 cd /texliveonfly_dir
-sharelatex tlmgr update --self
-sharelatex tlmgr install texliveonfly
+tlmgr update --self
+tlmgr install texliveonfly
 wget https://raw.githubusercontent.com/MatejSmycka/Docker-overleaf/main/main.tex
-sharelatex texliveonfly main.tex
+texliveonfly main.tex
